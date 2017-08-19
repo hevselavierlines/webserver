eec628fb9d691d0b5edb2619d03d50a305f7540f
Afaci <baumi494@gmail.com>
2017-08-19 19:48:03 +0200

    Initial Commit

---
+++ webservertest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  webservertest
+//
+//  Created by Manuel Baumgartner on 19/08/2017.
+//  Copyright © 2017 Hevselavier Lines Ver. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
